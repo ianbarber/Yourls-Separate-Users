58bde5bf95c6b92aee1fecba59396407498a04cb
Josh Panter <joshu@unfettered.net>
2018-09-04 20:41:24 -0400

    Update plugin.php
    -  update for YOURLS 1.7.3 (compatible with 1.7.2)
    -  restrict manage plugins Page from non-admin users #7
    -  added configurable protection of individual plugin pages from non-admins
    -  fixed global number of links and page numbers shown for each user #3
    -  fixed plugin URL in header

--- plugins/separate-users/plugin.php
+++ plugins/separate-users/plugin.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Separate Users
-Plugin URI: http://virgingroupdigital.wordpress.com
+Plugin URI: https://github.com/ianbarber/Yourls-Separate-Users
 Description: Allow some filtering of URLs based on the user that created them
-Version: 0.3
+Version: 0.4.0
 Author: Ian Barber <ian.barber@gmail.com>
 Author URI: http://phpir.com/
 */
@@ -42,9 +42,12 @@ if(!defined('SEPARATE_USERS_ADMIN_USER')) {
 
 yourls_add_action( 'insert_link', 'separate_users_insert_link' );
 yourls_add_action( 'activated_separate-users/plugin.php', 'separate_users_activated' );
+yourls_add_action( 'auth_successful', 'seperate_users_intercept_admin' );
 yourls_add_filter( 'admin_list_where', 'separate_users_admin_list_where' );
 yourls_add_filter( 'is_valid_user', 'separate_users_is_valid_user' );
 yourls_add_filter( 'api_url_stats', 'separate_users_api_url_stats' );
+yourls_add_filter( 'get_db_stats', 'separate_users_get_db_stats' );
+yourls_add_filter( 'admin_sublinks', 'separate_users_admin_sublinks' );
 
 /**
  * Activate the plugin, and add the user column to the table if not added
@@ -52,18 +55,35 @@ yourls_add_filter( 'api_url_stats', 'separate_users_api_url_stats' );
  * @param array $args 
  */
 function separate_users_activated($args) {
-        global $ydb; 
+	global $ydb; 
         
-        $table = YOURLS_DB_TABLE_URL;
-	$results = $ydb->get_results("DESCRIBE $table");
+	$table = YOURLS_DB_TABLE_URL;
+
+	if (version_compare(YOURLS_VERSION, '1.7.3') >= 0) {
+		$sql = "DESCRIBE `$table`";
+		$results = $ydb->fetchObjects($sql);
+	} else {
+		$results = $ydb->get_results("DESCRIBE $table");
+	}
+
 	$activated = false;
 	foreach($results as $r) {
-	        if($r->Field == 'user') {
-	                $activated = true;
-	        }
+		if($r->Field == 'user') {
+			$activated = true;
+		}
 	}
 	if(!$activated) {
-		$ydb->query("ALTER TABLE `$table` ADD `user` VARCHAR(255) NULL");
+
+		if (version_compare(YOURLS_VERSION, '1.7.3') >= 0) {
+			$binds = null;
+			$sql = "ALTER TABLE `$table` ADD `user` VARCHAR(255) NULL)";
+			$insert = $ydb->fetchAffected($sql,$binds);
+
+		} else {
+
+			$ydb->query("ALTER TABLE `$table` ADD `user` VARCHAR(255) NULL");
+
+		}
 	}
 }
 
@@ -75,15 +95,15 @@ function separate_users_activated($args) {
  * @return array
  */
 function separate_users_api_url_stats( $return, $shorturl ) {
-        $keyword = str_replace( YOURLS_SITE . '/' , '', $shorturl ); // accept either 'http://ozh.in/abc' or 'abc'
+	$keyword = str_replace( YOURLS_SITE . '/' , '', $shorturl ); // accept either 'http://ozh.in/abc' or 'abc'
 	$keyword = yourls_sanitize_string( $keyword );
-        $keyword = addslashes($keyword);
+	$keyword = addslashes($keyword);
         
-        if(separate_users_is_valid($keyword)) {
-                return $return;
-        } else {
-                return array('simple' => "URL is owned by another user", 'message' => 'URL is owned by another user', 'errorCode' => 403);
-        }
+	if(separate_users_is_valid($keyword)) {
+		return $return;
+	} else {
+		return array('simple' => "URL is owned by another user", 'message' => 'URL is owned by another user', 'errorCode' => 403);
+	}
 }
 
 
@@ -94,13 +114,13 @@ function separate_users_api_url_stats( $return, $shorturl ) {
  * @return bool is_valid
  */
 function separate_users_is_valid_user($is_valid) {
-        global $keyword; 
-        
-        if(!$is_valid || !defined("YOURLS_INFOS")) {
-                return $is_valid;
-        }
+	global $keyword; 
 
-        return separate_users_is_valid($keyword) ? true : "Sorry, that URL was created by another user."; 
+	if(!$is_valid || !defined("YOURLS_INFOS")) {
+		return $is_valid;
+	}
+
+	return separate_users_is_valid($keyword) ? true : "Sorry, that URL was created by another user."; 
 }
 
 /**
@@ -109,14 +129,21 @@ function separate_users_is_valid_user($is_valid) {
  * @param array $actions 
  */
 function separate_users_insert_link($actions) {
-        global $ydb; 
-        
-        $keyword = $actions[2];
-        $user = addslashes(YOURLS_USER); // this is pretty noddy, could do with some better filtering/checking
-        $table = YOURLS_DB_TABLE_URL;
-        
-        // Insert $keyword against $username
-        $result = $ydb->query("UPDATE `$table` SET  `user` = '" . $user . "' WHERE `keyword` = '" . $keyword . "'");
+	global $ydb; 
+
+	$keyword = $actions[2];
+	$user = addslashes(YOURLS_USER); // this is pretty noddy, could do with some better filtering/checking
+	$table = YOURLS_DB_TABLE_URL;
+
+	// Insert $keyword against $username
+	if (version_compare(YOURLS_VERSION, '1.7.3') >= 0) {
+		$binds = array( 'user' => $user,
+						'keyword' => $keyword);
+		$sql = "UPDATE `$table` SET  `user` = :user WHERE `keyword` = :keyword";
+		$result = $ydb->fetchAffected($sql, $binds);
+	} else {
+		$result = $ydb->query("UPDATE `$table` SET  `user` = $user WHERE `keyword` = $keyword");
+	}
 }
 
 /**
@@ -126,21 +153,142 @@ function separate_users_insert_link($actions) {
  * @return string
  */
 function separate_users_admin_list_where($where) {
-        $user = addslashes(YOURLS_USER); 
-		if(!is_serialized(SEPARATE_USERS_ADMIN_USER)) { 
-			if($user == SEPARATE_USERS_ADMIN_USER) {
-				return $where; // Allow admin user to see the lot. 
-			}
-		} else { 
-			$SEPARATE_USERS_ADMIN_USER = unserialize(SEPARATE_USERS_ADMIN_USER);
-				if(in_array($user, $SEPARATE_USERS_ADMIN_USER)) {
-					return $where; // Allow admin user to see the lot. 
-				}
+	$user = YOURLS_USER; 
+	if(!is_serialized(SEPARATE_USERS_ADMIN_USER)) { 
+		if($user == SEPARATE_USERS_ADMIN_USER) {
+			return $where; // Allow admin user to see the lot. 
 		}
+	} else { 
+		$SEPARATE_USERS_ADMIN_USER = unserialize(SEPARATE_USERS_ADMIN_USER);
+		if(in_array($user, $SEPARATE_USERS_ADMIN_USER)) {
+			return $where; // Allow admin user to see the lot. 
+		}
+	}
 
-        return $where . " AND (`user` = '$user' OR `user` IS NULL) ";
+	if (version_compare(YOURLS_VERSION, '1.7.3') >= 0) {
+		$where['sql'] = $where['sql'] . " AND (`user` = :user OR `user` IS NULL) ";
+		$where['binds']['user'] = $user;
+	}
+	else
+		$where = $where . " AND (`user` = $user OR `user` IS NULL) ";
+
+	return $where;
 }
 
+/**
+ * Filter out db stats to user access lvl
+ *
+ * @param array $return
+ * @param array $where
+ * @return array 
+ */
+function separate_users_get_db_stats( $return, $where ) {
+
+	$user = YOURLS_USER; 
+
+	// admin check
+	if(!is_serialized(SEPARATE_USERS_ADMIN_USER)) { 
+		if($user == SEPARATE_USERS_ADMIN_USER) {
+			return $return; // Allow admin user to see the lot. 
+		}
+	} else { 
+		$SEPARATE_USERS_ADMIN_USER = unserialize(SEPARATE_USERS_ADMIN_USER);
+		if(in_array($user, $SEPARATE_USERS_ADMIN_USER)) {
+			return $return; // Allow admin user to see the lot. 
+		}
+	}
+
+	// filter results
+	global $ydb;
+	$table_url = YOURLS_DB_TABLE_URL;
+
+	if (version_compare(YOURLS_VERSION, '1.7.3') >= 0) {
+
+		$where['sql'] = $where['sql'] . " AND (`user` = :user OR `user` IS NULL) ";
+		$where['binds']['user'] = $user;
+
+		$sql = "SELECT COUNT(keyword) as count, SUM(clicks) as sum FROM `$table_url` WHERE 1=1 " . $where['sql'];
+		$binds = $where['binds'];
+
+		$totals = $ydb->fetchObject($sql, $binds);
+
+	} else {
+
+		$where = $where . " AND (`user` = $user OR `user` IS NULL) ";
+		$totals = $ydb->get_results("SELECT COUNT(keyword) as count, SUM(clicks) as sum FROM `$table_url` WHERE 1=1 " . $where );
+	}
+
+	$return = array( 'total_links' => $totals->count, 'total_clicks' => $totals->sum );
+
+	return $return;
+
+}
+
+/**
+ * Restricting Access to Plugin Administration(s) for non-admins
+ *
+ *  
+ *
+ *
+ */
+function seperate_users_intercept_admin() {
+	// we use this GET param to send up a feedback notice to user
+    if ( isset( $_GET['access'] ) && $_GET['access']=='denied' ) {
+    	yourls_add_notice('Access Denied');
+    }
+
+	// only worry about this with HTML draw
+	if(!yourls_is_API()) {
+		$user = YOURLS_USER; 
+		// admin check
+		$admin = false;
+		if(!is_serialized(SEPARATE_USERS_ADMIN_USER)) { 
+			if($user == SEPARATE_USERS_ADMIN_USER)
+				$admin = true;
+		} else { 
+			$SEPARATE_USERS_ADMIN_USER = unserialize(SEPARATE_USERS_ADMIN_USER);
+			if(in_array($user, $SEPARATE_USERS_ADMIN_USER))
+				$admin = true;
+		}
+
+		// restrict access to non-admins
+		if( !$admin ) {
+			// intercept requests for plugin management page
+			if( isset( $_SERVER['REQUEST_URI'] ) && $_SERVER['REQUEST_URI'] == '/admin/plugins.php' ||
+				// intercept requests for plugin management 
+				isset( $_REQUEST['plugin'] ) ) {
+				yourls_redirect( yourls_admin_url( '?access=denied' ), 302 );
+			}
+		}
+	}
+}
+
+function separate_users_admin_sublinks( $links ) {
+	$user = YOURLS_USER; 
+	// admin check
+	$admin = false;
+	if(!is_serialized(SEPARATE_USERS_ADMIN_USER)) { 
+		if($user == SEPARATE_USERS_ADMIN_USER)
+			$admin = true;
+	} else { 
+		$SEPARATE_USERS_ADMIN_USER = unserialize(SEPARATE_USERS_ADMIN_USER);
+		if(in_array($user, $SEPARATE_USERS_ADMIN_USER))
+			$admin = true;
+	}
+
+	// restrict access to non-admins
+	if( !$admin ) {
+
+		$allowed = unserialize(SEPARATE_USERS_ALLOWED_PLUGIN_PAGES);
+		foreach( $links['plugins'] as $link => $ar ) {
+			if(!in_array($link, $allowed) )
+				unset($links['plugins'][$link]);
+		}
+	}
+
+	sort($links['plugins']);
+	return $links;
+}
 /**
  * Internal module function for testing user access to a keyword
  *
@@ -149,21 +297,28 @@ function separate_users_admin_list_where($where) {
  * @return boolean
  */
 function separate_users_is_valid( $keyword ) {
-        global $ydb; 
-        
-        $user = addslashes(YOURLS_USER);
-		if(!is_serialized(SEPARATE_USERS_ADMIN_USER)) { 
-			if($user == SEPARATE_USERS_ADMIN_USER) {
-				return true;
-			}
-		} else { 
-			$SEPARATE_USERS_ADMIN_USER = unserialize(SEPARATE_USERS_ADMIN_USER);
-			if(in_array($user, $SEPARATE_USERS_ADMIN_USER)) {
-				return true;
-			}
-		}        
-       
-        $table = YOURLS_DB_TABLE_URL;
-        $result = $ydb->query("SELECT 1 FROM `$table` WHERE  (`user` IS NULL OR `user` = '" . $user . "') AND `keyword` = '" . $keyword . "'");
-        return $result > 0;
+	global $ydb; 
+    
+    $user = addslashes(YOURLS_USER);
+	if(!is_serialized(SEPARATE_USERS_ADMIN_USER)) { 
+		if($user == SEPARATE_USERS_ADMIN_USER) {
+			return true;
+		}
+	} else { 
+		$SEPARATE_USERS_ADMIN_USER = unserialize(SEPARATE_USERS_ADMIN_USER);
+		if(in_array($user, $SEPARATE_USERS_ADMIN_USER)) {
+			return true;
+		}
+	}
+
+	$table = YOURLS_DB_TABLE_URL;
+
+	if (version_compare(YOURLS_VERSION, '1.7.3') >= 0) {
+		$binds = array( 'keyword' => $keyword, 'user' => $user);
+		$sql = "SELECT 1 FROM `$table` WHERE  (`user` IS NULL OR `user` = :user) AND `keyword` = :keyword";
+		$result = $ydb->fetchAffected($sql, $binds);
+	} else
+    	$result = $ydb->query("SELECT 1 FROM `$table` WHERE  (`user` IS NULL OR `user` = $user) AND `keyword` = $keyword");
+
+	return $result > 0;
 }
